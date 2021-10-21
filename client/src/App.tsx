b677c5cbcb63d35b782c16819e3f56f4976cfc4d
nguyenminhsang2004 <67231653+nguyenminhsang2004@users.noreply.github.com>
2021-10-21 15:25:30 +0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,28 @@
-import { NotFound, PrivateRoute } from 'components/Common';
-import { HomeLayout } from 'components/Layout';
-import LoginPage from 'features/auth/pages/LoginPage';
-import RegisterPage from 'features/auth/pages/RegisterPage';
-import React from 'react';
-import { Route, Switch } from 'react-router';
-import './App.css';
+import { NotFound, PrivateRoute } from 'components/Common'
+import { HomeLayout } from 'components/Layout'
+import LoginPage from 'features/auth/pages/LoginPage'
+import RegisterPage from 'features/auth/pages/RegisterPage'
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import './App.css'
 
 function App() {
   return (
     <Switch>
-
       <Route path="/login">
-        <LoginPage/>
+        <LoginPage />
       </Route>
-      
       <Route path="/register">
-        <RegisterPage/>
+        <RegisterPage />
       </Route>
-
       <PrivateRoute path="/home">
-        <HomeLayout/>
+        <HomeLayout />
       </PrivateRoute>
-
       <Route>
-        <NotFound/>
+        <NotFound />
       </Route>
-      
     </Switch>
-  );
+  )
 }
 
-export default App;
+export default App
